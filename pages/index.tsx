05db4e8aa79747b26d5d2c3a05e8f160fdf4bca6
k5953837 <k5953837@gmail.com>
2022-02-08 11:54:26 +0800

    Add hello world test words.
    Signed-off-by: k5953837 <k5953837@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <p>Hello World.</p>
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
